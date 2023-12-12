ab90f75f594c91916534ee3dd59dcb74f872e3d8
Ilya <vopi.vopi@mail.ru>
2023-12-12 15:49:00 +0300

    Changes

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,20 +2,21 @@ import React, { type FC } from 'react'
 import cls from './ThemeSwitcher.module.css'
 
 interface ThemeSwitcherProps { isOn: boolean, handleToggle: () => void }
-export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ isOn, handleToggle }) => {
-  localStorage.getItem('theme') === 'light' ? isOn = false : isOn = true
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({handleToggle }) => {
+
+  const themeIsOn = localStorage.getItem('theme') !== 'light';
 
   return (
     <>
       <input
-        checked={isOn}
+        checked={themeIsOn}
         onChange={handleToggle}
         className={cls.switch_checkbox}
         id='switch-new'
         type='checkbox'
       />
       <label
-        style={{ background: isOn ? 'var(--black-color)' : '' }}
+        style={{ background: themeIsOn ? 'var(--black-color)' : '' }}
         className={cls.switch_label}
         htmlFor='switch-new'
       >
