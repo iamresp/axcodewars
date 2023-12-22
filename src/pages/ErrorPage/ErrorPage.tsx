ab362940f00d0493c3141c143a32538b1e368887
ArtycoBW <artyco.bw@gmail.com>
2023-12-22 18:07:08 +0300

    [INT-36]:add title and button

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import cls from './ErrorPage.module.css'
 import Lottie from 'lottie-react'
 import animationData from '../../../public/error404.json'
+import { Button } from 'shared/components'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
     <div className={cls.main}>
-      <Lottie animationData={animationData} />
+      <Lottie animationData={animationData} style={{ height: 500 }} />
+      <h1 className={cls.title}>Страница не найдена!</h1>
+      <Link to={'/tasks'}>
+        <Button className={cls.btnRedirect} text='На главную' isOrange></Button>
+      </Link>
     </div>
 
   )
