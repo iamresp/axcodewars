b4cd882288481a1410f093e2b91767165905f0a2
ArtycoBW <artyco.bw@gmail.com>
2024-01-16 10:43:39 +0300

    [INT-43]:add loader

--- src/app/routing/routes.js
+++ src/app/routing/routes.js
@@ -1,17 +1,45 @@
-import React from 'react'
-import { AuthPage } from 'pages/AuthPage/AuthPage'
+import React, { Suspense } from 'react'
 import { Navigate } from 'react-router-dom'
-import { ProfilePage } from 'pages/ProfilePage/ProfilePage'
-import { ProfileEditPage } from 'pages/ProfileEditPage'
-import { ErrorPage } from 'pages/ErrorPage'
-import { TaskListPage } from 'pages/TasksPage'
-import { TaskPage } from 'pages/TaskPage'
-window.React = React
+import { Loading } from 'shared/components/Loading'
+
+const AuthPage = React.lazy(async () => await import('pages/AuthPage').then(module => ({
+  default: module.AuthPage
+}))
+)
+
+const ProfilePage = React.lazy(async () => await import('pages/ProfilePage').then(module => ({
+  default: module.ProfilePage
+}))
+)
+
+const ProfileEditPage = React.lazy(async () => await import('pages/ProfileEditPage').then(module => ({
+  default: module.ProfileEditPage
+}))
+)
+
+const TaskListPage = React.lazy(async () => await import('pages/TaskListPage').then(module => ({
+  default: module.TaskListPage
+}))
+)
+
+const TaskPage = React.lazy(async () => await import('pages/TaskPage').then(module => ({
+  default: module.TaskPage
+}))
+)
+
+const ErrorPage = React.lazy(async () => await import('pages/ErrorPage').then(module => ({
+  default: module.ErrorPage
+}))
+)
 
 export const publicRoutes = [
   {
     path: '/auth',
-    element: <AuthPage />
+    element: (
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <AuthPage />
+      </Suspense>
+    )
   },
   { path: '*', element: <Navigate to='/auth' replace /> }
 ]
@@ -19,23 +47,43 @@ export const publicRoutes = [
 export const userRoutes = [
   {
     path: '/tasks',
-    element: <TaskListPage />
+    element: (
+      <Suspense fallback={<Loading />}>
+        <TaskListPage />
+      </Suspense>
+    )
   },
   {
     path: '/tasks/:id',
-    element: <TaskPage />
+    element: (
+      <Suspense fallback={<Loading />}>
+        <TaskPage />
+      </Suspense>
+    )
   },
   {
     path: '/profile',
-    element: <ProfilePage />
+    element: (
+      <Suspense fallback={<Loading />}>
+        <ProfilePage />
+      </Suspense>
+    )
   },
   {
     path: '/profile/edit',
-    element: <ProfileEditPage />
+    element: (
+      <Suspense fallback={<Loading />}>
+        <ProfileEditPage />
+      </Suspense>
+    )
   },
   {
     path: '/error',
-    element: <ErrorPage />
+    element: (
+      <Suspense fallback={<Loading />}>
+        <ErrorPage />
+      </Suspense>
+    )
   },
   { path: '*', element: <Navigate to='/tasks' replace /> }
 ]
