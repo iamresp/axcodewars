426f03308a9673ba08875d3755e4671b09732b06
Peacemaker228 <alanchik99@gmail.com>
2024-01-19 15:26:10 +0300

    branch init with router fixes

--- src/app/routing/AppRouter.tsx
+++ src/app/routing/AppRouter.tsx
@@ -1,28 +1,23 @@
 import React, { type FC } from 'react'
-import { publicRoutes, userRoutes, type RoutesType } from './routes'
+import { publicRoutes, userRoutes } from './routes'
 import { Route, Routes } from 'react-router-dom'
-import { Loading } from 'shared/components/Loading'
 import { useAuth } from 'shared/hooks/useAuth'
 
 const AppRouter: FC = () => {
-  const { isLoading, isAuth } = useAuth()
+  const { isAuth } = useAuth()
 
-  const getRouts = (): RoutesType[] => {
+  const getRoutes = () => {
     if (!isAuth) {
       return publicRoutes
-    } else {
-      return userRoutes
     }
-  }
 
-  if (isLoading) {
-    return <Loading />
+    return userRoutes
   }
 
   return (
     <Routes>
-      {getRouts().map(route => (
-        <Route path={route.path} element={route.element} key={route.path} />
+      {getRoutes().map(route => (
+        <Route path={route.path} element={route.element} key={route.path}/>
       ))}
     </Routes>
   )
